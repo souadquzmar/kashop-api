31296b8c4b51184ceb9b456fafcfcc1b2c00df2c
souad <souadquzmar13@gmail.com>
2026-03-06 23:03:44 +0200

    identity

--- KASHOP.DAL/Data/ApplicationDbContext.cs
+++ KASHOP.DAL/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KASHOP.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KASHOP.DAL.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Category> Categories {get; set;}
         public DbSet<CategoryTranslation> CategoryTranslations {get; set;}
@@ -15,6 +17,13 @@ namespace KASHOP.DAL.Data
         {
             
         }
-        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+        }
     }
 }
---
+++ KASHOP.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace KASHOP.DAL.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName {get; set;}
+        public string? City {get; set;}
+        public string? Street {get; set;}
+    }
+}
