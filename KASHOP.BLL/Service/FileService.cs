35773702b6f32aded86f6cb14bbc6a59cd90d7ae
souad <souadquzmar13@gmail.com>
2026-03-29 01:12:50 +0300

    file upload

---
+++ KASHOP.BLL/Service/FileService.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace KASHOP.BLL.Service
+{
+    public class FileService : IFileService
+    {
+        public async Task<string?> UploadAsync(IFormFile file)
+        {
+            if(file != null && file.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                var filePath = Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "images",
+                    fileName
+                );
+                using(var stream = File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                return fileName;
+            }
+            return null;
+        }
+    }
+}
---
+++ KASHOP.BLL/Service/IFileService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace KASHOP.BLL.Service
+{
+    public interface IFileService
+    {
+        Task<string?> UploadAsync(IFormFile file);
+    }
+}
